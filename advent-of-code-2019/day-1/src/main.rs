0aadfca7f13096191af883e8409691eb6e003d7e
1u0 <7.prime+gh@gmail.com>
2019-12-07 04:11:03 +0100

    AoC-2019, solved: day 1, task 2

--- advent-of-code-2019/day-1/src/main.rs
+++ advent-of-code-2019/day-1/src/main.rs
@@ -1,5 +1,5 @@
-use std::{io, cmp};
 use std::io::BufRead;
+use std::{cmp, io};
 
 fn get_required_fuel(mass: i32) -> i32 {
     let mut mass = mass;
@@ -11,25 +11,21 @@ fn get_required_fuel(mass: i32) -> i32 {
     fuel
 }
 
+fn read_modules() -> Vec<i32> {
+    io::stdin()
+        .lock()
+        .lines()
+        .map(|line| line.unwrap().parse().unwrap())
+        .collect()
+}
+
 fn main() {
-    let mut total_fuel = 0;
-    let mut count = 0;
-    let stdin = io::stdin();
-    for line in stdin.lock().lines() {
-        let module_mass = line.unwrap()
-            .parse()
-            .unwrap();
-        count += 1;
-        total_fuel += get_required_fuel(module_mass);
+    let modules = read_modules();
+    let mut total_fuel: i64 = 0;
+    for module_mass in modules.iter() {
+        total_fuel += get_required_fuel(*module_mass) as i64;
     }
-
-//    let mut fuel_mass = total_fuel;
-//    while fuel_mass > 0 {
-//        fuel_mass = get_required_fuel(fuel_mass);
-//        total_fuel += fuel_mass;
-//    }
-
-    println!("Total fuel: {} ({})", total_fuel, count);
+    println!("Total fuel: {}", total_fuel);
 }
 
 #[cfg(test)]
@@ -38,14 +34,8 @@ mod tests {
 
     #[test]
     fn check_fuel_calculation() {
-        assert_eq!(0, get_required_fuel(7));
-        assert_eq!(0, get_required_fuel(8));
-        assert_eq!(1, get_required_fuel(9));
-        assert_eq!(1, get_required_fuel(10));
-        assert_eq!(1, get_required_fuel(11));
-        assert_eq!(2, get_required_fuel(12));
-        assert_eq!(2, get_required_fuel(13));
-
-//        assert_eq!(50346, get_required_fuel(100756));
+        assert_eq!(2, get_required_fuel(14));
+        assert_eq!(966, get_required_fuel(1969));
+        assert_eq!(50346, get_required_fuel(100756));
     }
 }
